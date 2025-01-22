93c9433d0024e7e2056ef1eeaea1c0840fbc6e73
laifugroup <laifuygj@gmail.com>
2025-01-22 17:17:37 +0800

    scrollView ok

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -3,16 +3,18 @@
 const app = getApp<IAppOption>()
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 
+interface AreaItem {
+  id: string;
+  name: string;
+  icon: string;
+  itemCount: number;
+  isEncrypted: boolean;
+}
+
 Component({
   data: {
-    motto: 'Hello World',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
-    },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+    areas: [] as AreaItem[],
+    page: 1,
   },
   methods: {
     // 事件处理函数
@@ -21,34 +23,81 @@ Component({
         url: '../logs/logs',
       })
     },
-    onChooseAvatar(e: any) {
-      const { avatarUrl } = e.detail
-      const { nickName } = this.data.userInfo
-      this.setData({
-        "userInfo.avatarUrl": avatarUrl,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-      })
+
+
+  
+    onSearch(e: any) {
+      const keyword = e.detail.value;
+      // TODO: 实现搜索逻辑
     },
-    onInputChange(e: any) {
-      const nickName = e.detail.value
-      const { avatarUrl } = this.data.userInfo
-      this.setData({
-        "userInfo.nickName": nickName,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-      })
+    async onPullDownRefresh() {
+      console.log("--onRefresh")
+      this.setData({ isRefreshing: true });
+      try {
+        // TODO: 刷新数据
+        await this.loadAreas(1);
+        this.setData({ page: 1 });
+      } finally {
+        this.setData({ isRefreshing: false });
+        wx.stopPullDownRefresh()
+      }
     },
-    getUserProfile() {
-      // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-      wx.getUserProfile({
-        desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-        success: (res) => {
-          console.log(res)
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+
+
+
+    async onReachBottom() {
+            // 延迟1秒后执行代码
+      setTimeout(function() {
+        console.log("延时1秒后执行");
+        // 在这里执行你需要的操作
+      }, 1000);
+
+      console.log("--onReachBottom")
+      try {
+        await this.loadAreas(this.data.page + 1);
+        this.setData({ page: this.data.page + 1 });
+      } finally {
+        wx.stopPullDownRefresh()
+        console.log("--stopPullDownRefresh")
+      }
+    },
+    async loadAreas(page: number) {
+
+       
+
+
+      console.log("page ---> "+page)
+      const mockAreas: AreaItem[] = [];
+        for (let i = 1; i <= 20; i++) {
+          mockAreas.push({
+          id:( page * 20 + i ).toString(),
+          name: i % 2 === 0 ? '书房'+( page * 20 + i ).toString() : '客厅' +( page * 20 + i ).toString(),
+          icon: '/assets/images/tab/' + (i % 2 === 0 ? 'checkin.png' : 'plus.png'),
+          itemCount: 8,
+          isEncrypted: i % 2 === 0
+          });
         }
-      })
+
+      if (page === 1) {
+        this.setData({ areas: mockAreas });
+      } else {
+        this.setData({ areas: [...this.data.areas, ...mockAreas] });
+      }
+      // 模拟是否还有更多数据
     },
+    onAreaTap(e: any) {
+      const areaId = e.currentTarget.dataset.id;
+      // TODO: 跳转到区域详情页
+    },
+    navigateToAdd() {
+      wx.navigateTo({
+        url: '/pages/add/index'
+      });
+    }
   },
+  lifetimes: {
+    attached() {
+      this.loadAreas(1);
+    }
+  }
 })
